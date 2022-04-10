b92eae4f0ce8845b0a5c6c6de513f728f66d06c8
Mårten Ward <Marten.ward@gmail.com>
2022-04-10 22:29:37 +0200

    Movie Controller Tests and Project cleanup
    + Added more Movie controller tests that mainly test the controllers abilities
    
    * Changed the project structure due to inconsistencies when commiting changes

--- src/test/java/com/example/demo/MovieControllerTest.java
+++ src/test/java/com/example/demo/MovieControllerTest.java
@@ -38,6 +38,7 @@ public class MovieControllerTest {
 	@Autowired
 	private ObjectMapper objectMapper;
 	
+	// Checks the default response from the movie controller with no parameters or ids
 	@Test
 	public void defaultResponse() throws Exception {
 		List<Movie> mockMovieList = createMockList(range(0,4));
@@ -51,22 +52,54 @@ public class MovieControllerTest {
 		verify(service).getMoviesById(null, 0, 5, "id", "asc");
 	}
 	
+	//Check that the controller responds to page
 	@Test
-	public void checkAsc() throws Exception {
-		List<Movie> mockMovieList = createMockList(range(0,4));
-		when(service.getMoviesById(null, 0, 5, "id", "asc")).thenReturn(mockMovieList);
+	public void checkPage() throws Exception {
+		List<Movie> mockMovieList = createMockList(range(3,8));
+		System.out.println(mockMovieList.size());
+		when(service.getMoviesById(null, 1, 5, "id", "asc")).thenReturn(mockMovieList);
 		
 		final String expectedResponseContent = objectMapper.writeValueAsString(mockMovieList);
-		this.mockMvc.perform(get("/api/v1/movies"))
+		this.mockMvc.perform(get("/api/v1/movies?page=1"))
 					.andDo(print())
 					.andExpect(status().isOk())
 					.andExpect(content().json(expectedResponseContent));
-		verify(service).getMoviesById(null, 0, 5, "id", "asc");
+		verify(service).getMoviesById(null, 1, 5, "id", "asc");
 	}
-	
+
+	//Check that the controller responds to elem
 	@Test
-	public void checkDesc() throws Exception {
-		List<Movie> mockMovieList = createMockList(reverse(range(0,4)));
+	public void checkElem() throws Exception {
+		List<Movie> mockMovieList = createMockList(range(0,2));
+		System.out.println(mockMovieList.size());
+		when(service.getMoviesById(null, 0, 3, "id", "asc")).thenReturn(mockMovieList);
+		
+		final String expectedResponseContent = objectMapper.writeValueAsString(mockMovieList);
+		this.mockMvc.perform(get("/api/v1/movies?elem=3"))
+					.andDo(print())
+					.andExpect(status().isOk())
+					.andExpect(content().json(expectedResponseContent));
+		verify(service).getMoviesById(null, 0, 3, "id", "asc");
+	}
+
+	// Checks that the controller responds to sortName
+	@Test
+	public void checkSortName() throws Exception {
+		List<Movie> mockMovieList = createMockList(range(0,4));
+		when(service.getMoviesById(null, 0, 5, "name", "asc")).thenReturn(mockMovieList);
+		
+		final String expectedResponseContent = objectMapper.writeValueAsString(mockMovieList);
+		this.mockMvc.perform(get("/api/v1/movies?sortName=name"))
+					.andDo(print())
+					.andExpect(status().isOk())
+					.andExpect(content().json(expectedResponseContent));
+		verify(service).getMoviesById(null, 0, 5, "name", "asc");
+	}
+
+	// Checks that the controller responds to order by 
+	@Test
+	public void checkOrderBy() throws Exception {
+		List<Movie> mockMovieList = createMockList(range(0,4));
 		when(service.getMoviesById(null, 0, 5, "id", "desc")).thenReturn(mockMovieList);
 		
 		final String expectedResponseContent = objectMapper.writeValueAsString(mockMovieList);
@@ -76,7 +109,8 @@ public class MovieControllerTest {
 					.andExpect(content().json(expectedResponseContent));
 		verify(service).getMoviesById(null, 0, 5, "id", "desc");
 	}
-	
+
+	// Checks that the controller responds to randomIds
 	@Test
 	public void checkRandomIds() throws Exception {
 		List<Integer> ids = List.of(1,3,5,7);
@@ -89,6 +123,21 @@ public class MovieControllerTest {
 					.andExpect(status().isOk())
 					.andExpect(content().json(expectedResponseContent));
 		verify(service).getMoviesById(Set.copyOf(ids), 0, 5, "id", "asc");
+	}
+	
+	// Checks that the controller responds to just one id with the get movie by id function
+	@Test
+	public void checkId() throws Exception {
+		Movie mockMovie = new Movie(LocalDate.of(2021, 12, 15),	"Spider-Man: No Way Home",	"Peter Parker is unmasked and no longer able to separate his normal life from the high-stakes of being a super-hero. When he asks for help from Doctor Strange the stakes become even more dangerous, forcing him to discover what it truly means to be Spider-Man.",	5084,	8940,	8,	"en",	"https://image.tmdb.org/t/p/original/1g0dhYtq4irTY1GPXvft6k4YLjm.jpg");
+		mockMovie.setId(0);
+		when(service.getMovieById(0)).thenReturn(mockMovie);
+		
+		final String expectedResponseContent = objectMapper.writeValueAsString(mockMovie);
+		this.mockMvc.perform(get("/api/v1/movies/0"))
+					.andDo(print())
+					.andExpect(status().isOk())
+					.andExpect(content().json(expectedResponseContent));
+		verify(service).getMovieById(0);
 	}
 	// Helper Functions
 	private List<Integer> range(int start, int end) {
